73e609957bab7fcc2389682c3e65f0527cc1387b
Chenzhan Shang <zhenbomy@foxmail.com>
2025-08-17 13:24:15 +0800

    Update action

--- page/.next/server/server-reference-manifest.js
+++ page/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kP7l73IJKzhbJ+dYYgsVTKb5eQcShAXHZZ9ppAndxUQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
